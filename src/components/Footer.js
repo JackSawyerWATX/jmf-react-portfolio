5d84fb22093986932be589d4598337d355e58ce3
John Doe <jack.h.sawyer@pm.me>
2024-01-03 09:26:52 -0800

    Updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@
 export default function Footer() {
     return (
         <section className="p-10 bg-indigo-200 flex justify-center">
-            <p className="font-bold">
-                ©Copyright 2023. All rights are reserved by Jack Sawyer Software
+            <p className="font-thin">
+                ©Copyright 2024. All rights reserved.
             </p>
         </section>
     );
