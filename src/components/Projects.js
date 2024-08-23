758b0efdc623f443d7af9a62954f90992c94952f
John Doe <jack.h.sawyer@pm.me>
2024-08-23 14:25:14 -0500

    Updated blog project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,7 +24,7 @@ export default function Projects() {
                             {dsaTrackerImage}
                             alt="Project 1"
                             className="w-full h-full 
-										bg-cover rounded" target="_blank"/>
+										bg-cover rounded" target="_blank" />
                     </a>
                 </div>
                 <div className="flex flex-col align-center mx-auto gap-4 justify-center flex-1">
@@ -36,12 +36,12 @@ export default function Projects() {
                         </a>
                     </h2>
                     <p>
-                        This is a project built on react which tracks your progess learning 
+                        This is a project built on react which tracks your progess learning
                         Data Structures and Algorithms and stores that in the local storage.
                     </p>
                 </div>
             </div>
-            
+
             <div className="p-10 bg-indigo-200 flex flex-col justify-center items-center gap-5 
 							lg:flex-row-reverse max-w-5xl mx-auto shadow transition hover:shadow-xl 
 							hover:scale-[105%] w-full">
@@ -63,7 +63,7 @@ export default function Projects() {
                         </a>
                     </h2>
                     <p>
-                        A movie search app, quite a bit more basic than IMDb, 
+                        A movie search app, quite a bit more basic than IMDb,
                         but the information returned is using an IMDb API.
                     </p>
                 </div>
@@ -74,7 +74,7 @@ export default function Projects() {
 							hover:scale-[105%] w-full">
                 <div className="w-80 rounded transition hover:scale-[105%] hover:shadow-sm">
                     <a href=
-                        "http://django-env.eba-imtfme86.us-west-2.elasticbeanstalk.com/"
+                        "https://master--jonchron.netlify.app/"
                         className="w-full h-full" target="_blank">
                         <img src=
                             {DjangoBlogImage}
@@ -85,14 +85,13 @@ export default function Projects() {
                 <div className="flex flex-col align-center mx-auto gap-4 justify-center flex-1">
                     <h2 className="font-bold text-3xl text-center transition hover:scale-[105%] ">
                         <a className="hover:underline" href=
-                            "http://django-env.eba-imtfme86.us-west-2.elasticbeanstalk.com/" target="_blank">
+                            "https://master--jonchron.netlify.app/" target="_blank">
                             Personal Blog
                         </a>
                     </h2>
                     <p>
-                        This is a blog page written in Python using Django framwork. Django is 
-                        a python framework that includes a user login to maintain and edit 
-                        complex, database driven websites. 
+                        This is a blog page written in JavaScript and JSX using Gatsby framwork.
+                        It utilizes a  a continious deplyoment on the netlify app.
                     </p>
                 </div>
             </div>
@@ -118,8 +117,8 @@ export default function Projects() {
                         </a>
                     </h2>
                     <p>
-                        This is a clone of the website for the Landon Hotel in West London. The original 
-                        page can be found at https://www.landonhotel.com. I built this to learn ReactJS 
+                        This is a clone of the website for the Landon Hotel in West London. The original
+                        page can be found at https://www.landonhotel.com. I built this to learn ReactJS
                         and is the first site I built using this framework.
                     </p>
                 </div>
@@ -141,7 +140,7 @@ export default function Projects() {
                 <div className="flex flex-col align-center mx-auto gap-4 justify-center flex-1">
                     <h2 className="font-bold text-3xl text-center transition hover:scale-[105%] ">
                         <a className="hover:underline" href=
-                            "https://main.d33zr5pqj7f3ln.amplifyapp.com/" 
+                            "https://main.d33zr5pqj7f3ln.amplifyapp.com/"
                             target="_blank">
                             Resume & Portfolio Site version 1.0
                         </a>
